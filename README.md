4caba781f7ec947da2b31d146dbd923226d2904a
Moritz <moritz@dialogik.tv>
2021-03-03 23:34:53 +0100

    Add !fx feature

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # A Twitch Chat WLED Controller
 
-Control a WLED using Twitch chat commands. This app currently supports setting the primary color of a WLED using the [WLED HTTP request API](https://github.com/Aircoookie/WLED/wiki/HTTP-request-API).
+Control a WLED using Twitch chat commands.
 
-Say `!color #RRGGBB` in the [dialogikTV Twitch livestream chat](https://www.twitch.tv/dialogikTV) to set the primary color. This commands only accepts the color value as a 6 character hexadecimal (`0-9` and `A-F`) string.
+This web page listens to Twitch chat WLED commands that allow to control the WLED. This application uses [WLED HTTP request API](https://github.com/Aircoookie/WLED/wiki/HTTP-request-API).
+
+## Setting color
+
+Say `!color RRGGBB` (or `#RRGGBB`) in [hexadecimal notation](https://en.wikipedia.org/wiki/Hexadecimal) in the [dialogikTV Twitch livestream chat](https://www.twitch.tv/dialogikTV) to set the primary color. This command currently only accepts the color value as a 6 character hexadecimal (`0-9` and `A-F`) string, optionally prefixed by a hash `#`.
+
+## Setting effects
+
+You can modify the WLED effects using the `!fx` command. (Technically, these commands change [*presets*](https://github.com/Aircoookie/WLED/wiki/Presets), not [*effects*](https://github.com/Aircoookie/WLED/wiki/List-of-effects-and-palettes#effects)).
+
+Say `!fx <effect>`, where `effect` is
+
+* `android`,
+* `scanner` or
+* `breathe`
+
+to select a specific effect. You can also just say `!fx` to cycle through effects.
--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 const channel = 'dialogikTV';
 const { chat } = new window.TwitchJs({ channel });
+const effects = {
+    "android": 1,
+    "scanner": 2,
+    "breathe": 3
+}
+var currentEffect = 1;
+
+console.log('[SYS] WLED Chatbot Overlay initalized');
+
 chat.connect().then(() => {
     chat.join(channel);
+    console.log('[SYS] Channel joined');
+
+    // Reset WLED to default effect (android=1)
+    fetch(`http://192.168.2.30/win&PL=1`)
+        .then(() => {
+            console.log(`[SENT] WLED initialized with default effect`);
+        });
 }).catch((e) => { console.error('Twitch error in Promise', e); });
 // chat.on('PRIVMSG', (message) => {
 chat.on('PRIVMSG', (message) => {
     message = message.message;
-    if(message.startsWith('!color ') && message !== '!color ') {
+    if(message.startsWith('!color ') && message != '!color ') {
         let color = message.replace('!color ', '').replace('#', '');
-        console.log(`Requested color [${color}]`);
+        console.log(`[REQ] Requested color [${color}]`);
 
         // Only allow Mods, VIPs and Subs to change color
         // if(!(message.tags.mod == 1 || message.tags.badges.vip == 1 || message.tags.subscriber == 1)) {
@@ -16,14 +32,50 @@ chat.on('PRIVMSG', (message) => {
         // }
 
         // Ensure color is hex value
-        if(!(/^[0-9A-F]{6}$/i.test(color))) {
-            console.log(`Denying color request, [${color}] is no hex value`);
+        if(color.length == 6) {
+            if(!(/^[0-9A-F]{6}$/i.test(color))) {
+                console.log(`[ERR] Denying color request, [${color}] is no hex value`);
+                return;
+            }
+            
+            fetch(`http://192.168.2.30/win&CL=h${color}`)
+                .then(() => console.log(`[SENT] Color request [${color}] sent`));
+        } else {
+            console.log(`[ERR] No valid color [${color}] specified`);
+        }
+    }
+
+    // Select next effect
+    else if(message == '!fx') {
+        // Increase effect ID
+        currentEffect = currentEffect + 1;
+        if(currentEffect > effects.length) {
+            currentEffect = 1;
+        }
+
+        console.log(`[REQ] Next effect requested [${currentEffect}]`);
+
+        fetch(`http://192.168.2.30/win&PL=${currentEffect}`)
+            .then(() => {
+                console.log(`[SENT] Effect [ID ${currentEffect}] request sent`);
+            });
+    }
+
+    // Select specific preset/effect
+    else if(message.startsWith('!fx') && message != '!fx ') {
+        let effect = message.replace('!fx ', '');
+        console.log(`[REQ] Requested effect [${effect}]`);
+        if(!effect in effects) {
+            console.log(`[ERR] Denying effect request, [${effect}] is no valid effect`);
             return;
         }
 
-        // Todo: add more features!
-        // https://github.com/Aircoookie/WLED/wiki/HTTP-request-API
-        fetch(`http://192.168.2.122/win&CL=h${color}`)
-            .then(() => console.log(`Color [${color}] request sent`));
+        let effectId = effects[effect];
+        currentEffect = effectId;
+        console.log(`[REQ] Requested effect [${effect}] [ID ${effectId}]`);
+        fetch(`http://192.168.2.30/win&PL=${effectId}`)
+            .then(() => {
+                console.log(`[SENT] Effect [${effect}] [ID ${effectId}] request sent`);
+            });
     }
 });
